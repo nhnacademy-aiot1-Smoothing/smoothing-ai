8975cc230e728645df7beac2a2262bc75f040bba
김재혁 <rlawogur329@mail.ulsan.ac.kr>
2024-05-22 17:05:32 +0900

    [Feat]: 발전기 데이터를 위한 엔티티 클래스

---
+++ src/main/java/live/smoothing/ai/entity/PowerGenerationData.java
@@ -0,0 +1,24 @@
+package live.smoothing.ai.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+/**
+ * 발전 조회 API에 사용 될 Entity 클래스 입니다.
+ * 발전 데이터를 저장하는데 사용하고, InfluxDB에서 데이터를 조회할 때 사용합니다.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PowerGenerationData {
+
+    private Instant time;
+    private Double value;
+    private String field;
+    private String measurement;
+}
