b480709b87d2fd896a3c670a22b93530ab9f7efc
Maru375 <www980304@gmail.com>
2024-05-26 23:55:13 +0900

    [Feat]: 발전기 로그 서비스 구현
    - getPowerGeneratorLogs : 발전기 로그 조회
    - savePowerGeneratorLog : 발전기 로그 기록

---
+++ src/main/java/live/smoothing/ai/generatorlog/service/PowerGeneratorLogServiceImpl.java
@@ -0,0 +1,45 @@
+package live.smoothing.ai.generatorlog.service;
+
+import live.smoothing.ai.generator.entity.PowerGenerator;
+import live.smoothing.ai.generator.repository.PowerGeneratorRepository;
+import live.smoothing.ai.generatorlog.dto.PowerGeneratorLogResponse;
+import live.smoothing.ai.generatorlog.entity.PowerGeneratorLog;
+import live.smoothing.ai.generatorlog.repository.PowerGeneratorLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PowerGeneratorLogServiceImpl implements PowerGeneratorLogService {
+
+    private final PowerGeneratorRepository powerGeneratorRepository;
+    private final PowerGeneratorLogRepository powerGeneratorLogRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<PowerGeneratorLogResponse> getPowerGeneratorLogs(String generatorId) {
+
+        List<PowerGeneratorLog> powerGeneratorLogs = powerGeneratorLogRepository.findTop8ByPowerGenerator_GeneratorIdOrderByTimeAsc(generatorId);
+        return powerGeneratorLogs.stream()
+                .map(data -> new PowerGeneratorLogResponse(data.getTime(), data.getMessage()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void savePowerGeneratorLog(String generatorId, String message) {
+
+        PowerGenerator powerGenerator = powerGeneratorRepository.getReferenceById(generatorId);
+
+        powerGeneratorLogRepository.save(new PowerGeneratorLog(
+                powerGenerator,
+                LocalDateTime.now(),
+                message
+        ));
+    }
+}
