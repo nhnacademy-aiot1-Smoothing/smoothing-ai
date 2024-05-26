fe1a42b4dace4452d4a668325fca7fef8214200d
Maru375 <www980304@gmail.com>
2024-05-26 23:57:57 +0900

    [Refactor]: 코드 리팩토링

--- src/main/java/live/smoothing/ai/controller/PredictionDataController.java
+++ src/main/java/live/smoothing/ai/controller/PredictionDataController.java
@@ -3,6 +3,7 @@ package live.smoothing.ai.controller;
 import live.smoothing.ai.common.dto.InfluxDataResponse;
 import live.smoothing.ai.prediction.service.PredictionDataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +16,14 @@ import java.util.List;
 @RequestMapping("/api/ai")
 public class PredictionDataController {
 
-    private final PredictionDataService aiService;
+    private final PredictionDataService predictionDataService;
 
     @GetMapping
-    public List<InfluxDataResponse> getDailyPredictedPowerUsage(@RequestParam String measurement,
-                                                                    @RequestParam String field) {
+    public ResponseEntity<List<InfluxDataResponse>> getDailyPredictedPowerUsage(@RequestParam String measurement,
+                                                                                @RequestParam String field) {
 
-        return aiService.getPredictionData(measurement, field);
+        List<InfluxDataResponse> predictionData = predictionDataService.getPredictionData(measurement, field);
+
+        return ResponseEntity.ok(predictionData);
     }
 }
