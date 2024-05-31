0b2f9e88c4e7cd0e9677a35c653f7a89e262afd7
Maru375 <www980304@gmail.com>
2024-06-01 01:16:35 +0900

    [Feat]: 조회 시간 수정

--- src/main/java/live/smoothing/ai/actualuse/service/ActualUseDataServiceImpl.java
+++ src/main/java/live/smoothing/ai/actualuse/service/ActualUseDataServiceImpl.java
@@ -6,7 +6,10 @@ import live.smoothing.ai.actualuse.repository.ActualUseDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,11 +23,15 @@ public class ActualUseDataServiceImpl implements ActualUseDataService {
 
         List<ActualUseData> todayActualUseData = repository.getTodayActualUseData(location, description);
 
-        return todayActualUseData.stream()
-                .map(data -> {
-                    Double calculatedValue = (data.getValue() != null) ? Math.round(data.getValue() / 360 * 1000) / 1000.0 : null;
-                    return new InfluxDataResponse(data.getTime(), calculatedValue);
-                })
+        Map<Instant, Double> hourlySum = todayActualUseData.stream()
+                .collect(Collectors.groupingBy(
+                        data -> data.getTime().truncatedTo(ChronoUnit.HOURS),
+                        Collectors.summingDouble(data -> data.getValue() != null ? data.getValue() : 0.0)
+                ));
+
+        return hourlySum.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry -> new InfluxDataResponse(entry.getKey(), (double) Math.round(entry.getValue() / 60)))
                 .collect(Collectors.toList());
     }
 }
